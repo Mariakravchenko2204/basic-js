9c928a80cf7153d170e935361301aecc27b500a8
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2024-12-26 17:43:04 -0500

    feat: submit get-email-domain, hanoi-tower, mac-address solutions

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,10 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const regex = /[0-9A-F]/g;
+  let res = false;
+  const nArr = n.split("-");
+  for (let i = 0; i < nArr.length; i += 1) {
+    if(nArr[i].length !== 2) {
+      res = false;
+      break;
+    }
+    const match = nArr[i].match(regex);
+    if (match.length === 2) {
+      res = true;
+    } else {
+      res = false;
+      break;
+    }
+  }
+  return res;
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
